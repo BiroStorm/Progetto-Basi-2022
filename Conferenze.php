d6af49248ce97a2885f9bf4c17603c4ecb3ed72b
bottii <alessand.bottiglier2@studio.unibo.it>
2022-04-20 15:25:38 +0200

    hover sulle cards

--- Conferenze.php
+++ Conferenze.php
@@ -16,7 +16,11 @@ if (isset($_SESSION["username"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conferenze</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <style>
+    .card:hover {
+      background-color: #f2f2f2 !important;
+    }
+    </style>
 </head>
 
 <body>
